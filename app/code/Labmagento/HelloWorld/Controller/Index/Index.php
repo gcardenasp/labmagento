5501f1de245ca348924af5a0b2f1013c6622b25b
Germán <german2779@gmail.com>
2026-02-06 10:07:42 -0500

    Add Labmagento_HelloWorld module with basic Hello World page

---
+++ app/code/Labmagento/HelloWorld/Controller/Index/Index.php
@@ -0,0 +1,20 @@
+<?php
+namespace Labmagento\HelloWorld\Controller\Index;
+
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\Controller\ResultFactory;
+
+class Index extends Action
+{
+    public function __construct(Context $context)
+    {
+        parent::__construct($context);
+    }
+
+    public function execute()
+    {
+        $resultPage = $this->resultFactory->create(ResultFactory::TYPE_PAGE);
+        return $resultPage;
+    }
+}
